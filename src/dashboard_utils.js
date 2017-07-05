6b003368832c32bce869b40a40dcfa3799e0595e
mccarvik <mccarviks@gmail.com>
2017-07-05 20:15:34 +0000

    need to find javascript source for live historical data

--- src/dashboard_utils.js
+++ src/dashboard_utils.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+var googleFinance = require('google-finance');
 
 export function ticker_setup() {
     var ticker_groups = [];
@@ -24,12 +25,11 @@ export function get_values(tick) {
         @return data (obj) - dictionary with 52 week hi, 52 week low, current px, and %change from yesterday)
     */
     var data = getData(tick);
-    console.log(data);
-    return null;
+    return data;
 }
 
 function getData(symbol) {
-    var endDate = new Date()
+    var endDate = new Date();
     var startDate = new Date();
     startDate.setYear(endDate.getFullYear() - 1);
     endDate = (endDate.getMonth() + 1) + '' + endDate.getDate() + '' + endDate.getFullYear();
@@ -37,47 +37,64 @@ function getData(symbol) {
     
     var url_live = 'http://finance.google.com/finance/info?client=ig&q=' + symbol;
     var url_hist = 'http://www.google.com/finance/historical?q=' + symbol + '&startdate=' + startDate + '&enddate=' + endDate + '&output=csv';
-    console.log(url_hist);
-    console.log(url_live);
+    // console.log(url_hist);
+    // console.log(url_live);
     var live;
     var hist;
     
-    // LOOK UP CORS
-    
+    // JSONP used to work around Cross Origin Resource Sharing Problem
+    console.log("bookend");
+    $.ajax({
+        url: url_live,
+        dataType: 'jsonp',
+        success: function(dataWeGotViaJsonp){
+            console.log(dataWeGotViaJsonp);
+            live = dataWeGotViaJsonp;
+        }
+    });
     
     $.ajax({
-        type: "GET",
         url: url_hist,
-        dataType: "text",
-        success: function(data) {
-            hist = processData(data);
+        dataType: 'jsonp',
+        success: function(dataWeGotViaJsonp){
+            console.log('here');
+            console.log(dataWeGotViaJsonp);
+            hist = dataWeGotViaJsonp;
         }
     });
-    console.log(hist);
     
-    $.getJSON(url_live).then((data) => {
-        live = data;
-    });
-    console.log(live);
+    console.log("bookend");
+    return;
+    
+    // LOOK UP CORS
+    
+    // fetch(url_live, { 
+    //   method: 'GET',
+    //   headers:{
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Credentials':true,
+    //     'Access-Control-Allow-Methods':'POST, GET'
+    //   }
+    // })
+    // .then(function(response) {
+    //     live = response;
+    // }).then(console.log(live));
+    // console.log("bookend");
+    // return;
+    
+    
+    // $.ajax({
+    //     type: "GET",
+    //     url: url_hist,
+    //     dataType: "text",
+    //     success: function(data) {
+    //         hist = processData(data);
+    //     }
+    // });
+    // console.log(hist);
+    
+    // $.getJSON(url_live).then((data) => {
+    //     live = data;
+    // });
+    // console.log(live);
 }
-
-
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length === headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
-        }
-    }
-    return lines;
-}
-
