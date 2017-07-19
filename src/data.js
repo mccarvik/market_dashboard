78181d3d0dd0ee0d4fe57719eef4a5a0f8e98661
mccarvik <mccarviks@gmail.com>
2017-07-19 19:46:24 +0000

    added meat an live stock commodities, would love to add more interest rate stuff or more global stuff but limited by yahoo

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@
 export var asset_classes = {
     'Equities' : ['US Equities', 'Global Equities'],
     'Rates' : ['US Rates', 'Other'],
-    'Commodities' : ['Energy', 'Metals', 'Agriculture'],
+    'Commodities' : ['Energy', 'Metals', 'Agriculture', 'Meat / Live Stock'],
     'Currencies' : ['G10 Currencies', 'EM Currencies'],
 };
 
@@ -20,8 +20,8 @@ export var raw_data = {
         'USD/JPY' : ['JPY=X', 'yahoolive', 'USDJPY', 'quandl_currfx', 1],
         'USD/GBP' : ['GBP=X', 'yahoolive', 'USDGBP', 'quandl_currfx', 1],
         'USD/CAD' : ['CAD=X', 'yahoolive', 'USDCAD', 'quandl_currfx', 1],
-        // 'USD/CHF' : ['CHF=X', 'yahoolive', 'USDCHF', 'quandl_currfx', 1],
-        // 'USD/AUD' : ['AUD=X', 'yahoolive', 'USDAUD', 'quandl_currfx', 1],
+        'USD/CHF' : ['CHF=X', 'yahoolive', 'USDCHF', 'quandl_currfx', 1],
+        'USD/AUD' : ['AUD=X', 'yahoolive', 'USDAUD', 'quandl_currfx', 1],
         // 'USD/NZD' : ['NZD=X', 'yahoolive', 'USDNZD', 'quandl_currfx', 1],
         // 'USD/SEK' : ['SEK=X', 'yahoolive', 'USDSEK', 'quandl_currfx', 1],
         // 'USD/NOK' : ['NOK=X', 'yahoolive', 'USDNOK', 'quandl_currfx', 1],
@@ -31,11 +31,11 @@ export var raw_data = {
         'USD/MXN' : ['USDMXN=X', 'yahoolive', 'USDMXN', 'quandl_currfx', 1],
         'USD/BRL' : ['BRL=X', 'yahoolive', 'USDBRL', 'quandl_currfx', 1],
         'USD/CNY' : ['CNY=X', 'yahoolive', 'USDCNY', 'quandl_currfx', 1],
-        // 'USD/SGD' : ['SGD=X', 'yahoolive', 'USDSGD', 'quandl_currfx', 1],
+        'USD/SGD' : ['SGD=X', 'yahoolive', 'USDSGD', 'quandl_currfx', 1],
         // 'USD/HKD' : ['HKD=X', 'yahoolive', 'USDHKD', 'quandl_currfx', 1],
         // 'USD/ZAR' : ['USDZAR=X', 'yahoolive', 'USDZAR', 'quandl_currfx', 1],
         // 'USD/TRY' : ['TRY=X', 'yahoolive', 'USDTRY', 'quandl_currfx', 1],
-        // 'USD/KRW' : ['KRW=X', 'yahoolive', 'USDKRW', 'quandl_currfx', 1],
+        'USD/KRW' : ['KRW=X', 'yahoolive', 'USDKRW', 'quandl_currfx', 1],
         // 'USD/RUB' : ['RUB=X', 'yahoolive', 'USDRUB', 'quandl_currfx', 1],
         // 'USD/INR' : ['INR=X', 'yahoolive', 'USDINR', 'quandl_currfx', 1],
         // 'USD/IDR' : ['IDR=X', 'yahoolive', 'USDIDR', 'quandl_currfx', 1],
@@ -65,17 +65,22 @@ export var raw_data = {
     'Metals' : {
         'Gold' : ['GC=F', 'yahoolive', 'CME_GC1', 'quandl_chris', 4],
         'Silver' : ['SI=F', 'yahoolive', 'CME_SI1', 'quandl_chris', 4],
-        // 'Platinum' : ['PL=F', 'yahoolive', 'PLAT', 'quandl_llpm', 4],
-        'Copper' : ['HG=F', 'yahoolive', 'CME_HG1', 'quandl_chris', 4]
+        'Copper' : ['HG=F', 'yahoolive', 'CME_HG1', 'quandl_chris', 4],
+        // 'Platinum' : ['PL=F', 'yahoolive', 'PLAT', 'quandl_llpm', 4]
     },
     'Agriculture' : {
         'Corn' : ['C=F', 'yahoolive', 'CME_C1', 'quandl_chris', 4],
         'Sugar' : ['SB=F', 'yahoolive', 'ICE_SB1', 'quandl_chris', 4],
         'Wheat' : ['W=F', 'yahoolive', 'CME_W1', 'quandl_chris', 4],
         // 'Soybean' : ['S=F', 'yahoolive', 'CME_S1', 'quandl_chris', 4],
-        'Cotton' : ['CT=F', 'yahoolive', 'ICE_CT1', 'quandl_chris', 4],
+        // 'Cotton' : ['CT=F', 'yahoolive', 'ICE_CT1', 'quandl_chris', 4],
         // 'Cocoa' : ['CC=F', 'yahoolive', 'ICE_CC1', 'quandl_chris', 4],
         'Coffee' : ['KC=F', 'yahoolive', 'ICE_KC1', 'quandl_chris', 4]
+    },
+    'Meat / Live Stock' : {
+        'Live Cattle' : ['LC=F', 'yahoolive', 'CME_LC1', 'quandl_chris', 4],
+        'Lean Hogs' : ['LH=F', 'yahoolive', 'CME_LN1', 'quandl_chris', 4],
+        // 'Feeder Cattle' : ['FC=F', 'yahoolive', 'CME_FC1', 'quandl_chris', 4],
     },
     'US Rates' : {
         '3M' : ['^IRX', 'yahoolive', 'YIELD', 'quandl_tsy', 2],
