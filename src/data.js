3c0ec8d7921c503e938567aeb58bc06e80b95ec7
Kevin McCarville <mccarviks@gmail.com>
2021-08-30 08:52:31 -0500

    big changes to add google finance

--- src/data.js
+++ src/data.js
@@ -54,7 +54,7 @@ export var raw_data = {
         "UK - FTSE" : ["^FTSE", "yahoolive", "LIFFE_Z1", "quandl_chris", 4, 0.75],
         "Germany - DAX" : ["^GDAXI", "yahoolive", "EUREX_FDAX1", "quandl_chris", 4, 0.75],
         "France - CAC" : ["^FCHI", "yahoolive", "LIFFE_MFC1", "quandl_chris", 4, 0.75],
-        "Canada - S&P/TSX" : ["^GSPTSE", "yahoolive", "MX_SCF1", "quandl_chris", 7, 0.75],
+        "Canada - S&P/TSX" : ["^GSPTSE", "yahoolive", "MX_SCF1", "quandl_chris", 4, 0.75],
         "Japan - Nikkei" : ["^N225", "yahoolive", "CME_NK1", "quandl_chris", 4, 0.75],
         "China - Shanghai Composite" : ["000001.SS", "yahoolive", "", "quandl_chris", 4, 0.75],
         "Hong Kong - Hang Seng" : ["^HSI", "yahoolive", "HKEX_HSI1", "quandl_chris", 4, 0.75],
--- src/scrape_live_data.py
+++ src/scrape_live_data.py
@@ -5,12 +5,9 @@ import time, pdb, json, csv, datetime, requests, signal
 from functools import wraps
 # import pandas as pd
 from bs4 import BeautifulSoup
-# from pyvirtualdisplay import Display
-# from selenium import webdriver
 
-
-# Dont think I need selenium but good to know
-
+# for currency data
+# https://blog.quandl.com/api-for-currency-data
 # quandl_chris_root = 'https%3A//www.quandl.com/api/v3/datasets/CHRIS/{}.json%3Fapi_key%3D{}%26start_date%3D{}';
 quandl_chris_root = 'https://www.quandl.com/api/v3/datasets/CHRIS/{}.json?api_key={}&start_date={}'
 API_KEY = 'J4d6zKiPjebay-zW7T8X'
@@ -33,49 +30,87 @@ def timeout(seconds=10, error_message="API CALL TOOK TOO LONG"):
     return decorator
 
 
-@timeout(60)
-def get_stock_price(name):
-    # display = Display(visible=0, size=(1024, 768))
-    # display.start()
-    # browser = webdriver.Firefox()
-    # actions = webdriver.ActionChains(browser)
+def get_goog_stock_price(name):
+    """
+    Get the google price of a stock ticker from google finance
+    """
+    resp = requests.get('https://www.google.com/finance/quote/' + name)
+    resp_doc = resp.content
+    soup = BeautifulSoup(resp_doc, 'html.parser')
+
+    # Need this for diff values if stock is up or down
+    try:
+        chg = reformatGoogChg(soup.find_all("span", class_="Z63m9d yoGq8")[1].get_text())
+    except Exception as e:
+        chg = None
+        # <span class="Z63m9d yoGq8">
+    try:
+        val = soup.find("div", class_="YMlKec fxKbKc").get_text()
+        val = val.replace("$","")
+    except:
+        val = None
+
+    try:
+        if name in ['XTL:NYSEARCA', "VWO:NYSEARCA", "VPL:NYSEARCA", "VRP:NYSEARCA"]:
+            rng = soup.findAll("div", class_="P6K39c")[2].get_text().replace("$","")
+        else:    
+            matches = soup.findAll("div", class_="P6K39c")[0]
+            for m in matches:
+                rng = m.get_text()
+        lo, hi = rng.split(" - ")
+    except Exception as e:
+        hi=None
+        lo=None
+
+    return [val, chg, lo, hi]
+
+# @timeout(60)
+def get_stock_price(name, results):
     # browser.get('https://finance.yahoo.com/quote/' + name + '?p=' + name)
     # time.sleep(5) # sleep for 5 seconds
     # content = browser.find_element_by_id('content') # Error on this line
     # resp = requests.get('https://finance.yahoo.com/quote/' + name + '?p=' + name)
     http = url.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=cert.where())
     html_doc = http.request('GET', 'https://finance.yahoo.com/quote/' + name + '?p=' + name)
-    if name == 'DJIA':
-        pdb.set_trace()
-        print()
     soup = BeautifulSoup(html_doc.data, 'html.parser')
     
     # Need this for diff values if stock is up or down
-    try:
-        chg = reformatChg(soup.find("span", class_="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)").get_text())
-    except Exception as e:
+    if results[1] is None:
         try:
-            chg = reformatChg(soup.find("span", class_="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)").get_text())
-        except Exception as e2:
+            chg = reformatChg(soup.find("span", class_="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)").get_text())
+        except Exception as e:
             try:
-                chg = reformatChg(soup.find("span", class_="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($dataBlack)").get_text())
-            except Exception as e3:
-                chg = 0
-                
-    try:
-        val = soup.find("span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").get_text()
-    except:
-        val = 0
+                chg = reformatChg(soup.find("span", class_="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)").get_text())
+            except Exception as e2:
+                try:
+                    chg = reformatChg(soup.find("span", class_="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($dataBlack)").get_text())
+                except Exception as e3:
+                    chg = 0
+    else:
+        chg = results[1]
     
-    try:
-        matches = soup.findAll("td", class_="Ta(end) Fw(600) Lh(14px)")
-        for m in matches:
-            if "FIFTY_TWO_WK_RANGE" in str(m):
-                rng = m.get_text()
-        lo, hi = rng.split(" - ")
-    except Exception as e:
-        hi=0
-        lo=0
+    if results[0] is None:
+        try:
+            val = soup.find("span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").get_text()
+        except:
+            val = 0
+    else:
+        val = results[0]
+    
+    if results[2] is None or results[3] is None:
+        try:
+            matches = soup.findAll("td", class_="Ta(end) Fw(600) Lh(14px)")
+            for m in matches:
+                if "FIFTY_TWO_WK_RANGE" in str(m):
+                    rng = m.get_text()
+            lo, hi = rng.split(" - ")
+        except Exception as e:
+            hi=None
+            lo=None
+    else:
+        lo = results[2]
+        hi = results[3]
+
     return [val, chg, lo, hi]
 
     
@@ -86,16 +121,35 @@ def reformatChg(chg):
     return chg
 
 
-@timeout(30)
-def get_quandl_data(ticker, t_ind):
+def reformatGoogChg(chg):
+    remove = ['(', ')', '%', '+', "-"]
+    chg = round(float("".join([c for c in chg if c not in remove])), 2)
+    return chg
+
+
+# @timeout(30)
+def get_quandl_data(ticker, t_ind, t_src):
     start_date = (datetime.datetime.today() - datetime.timedelta(days=365)).date()
-    url = quandl_chris_root.format(ticker, API_KEY, start_date)
+    if t_src == 'quandl_boefx':
+        root = "http://www.quandl.com/api/v3/datasets/BOE/{}?api_key={}&start_date={}"
+        url = root.format(ticker, API_KEY, start_date)
+    elif t_src == 'quandl_fredfx':
+        root = "http://www.quandl.com/api/v3/datasets/FRED/{}?api_key={}&start_date={}"
+        url = root.format(ticker, API_KEY, start_date)
+    else:
+        root = quandl_chris_root
+        url = root.format(ticker, API_KEY, start_date)
     try:
         data = json.loads(requests.get(url).content)
         data = data['dataset']['data']
         data = [d[t_ind] for d in data if d[t_ind is not None]]
+        data = [d for d in data if d is not None]
+        # exception as we cant find USD/EUR, only EUR/USD
+        if ticker == 'DEXUSEU':
+            data = [1/d for d in data]
         return min(data), max(data)
     except Exception as e:
+        print(e)
         if 'quandl_error' in data.keys():
             print("Hitting API too much: " + str(t[0]))
         else:
@@ -104,23 +158,26 @@ def get_quandl_data(ticker, t_ind):
 
 
 if __name__ == '__main__':
-    with open('/home/ec2-user/environment/market_dashboard/src/raw_data.json') as data_file:    
+    with open('C:\\Users\\19739\\Python\\market_dashboard\\src\\raw_data.json') as data_file:    
         data = json.load(data_file)
         data = data['raw_data']
     tickers = []
     
     for k, v in data.items():
-        tickers += [(val[0], val[1][0], val[1][2], val[1][4]) for val in v.items()]
-
+        tickers += [(val[0], val[1][0], val[1][2], val[1][4], val[1][5], val[1][3]) for val in v.items()]
+        
     live_data = {}
     for t in tickers:
         try:
+            results = get_goog_stock_price(t[4])
+            # results = get_goog_stock_price(t[4])
+
             # need to write this to outfile
-            results = get_stock_price(t[1])
+            results = get_stock_price(t[1], results)
             
-            # need this for reaching quandl for tickers that dont have min max off of yahoo
-            if results[2] == 0 and results[3] == 0:
-                results[2], results[3] = get_quandl_data(t[2], t[3])
+            # need this for reaching quandl for tickers that dont have min max off of yahoo or google
+            if results[2] is None and results[3] is None:
+                results[2], results[3] = get_quandl_data(t[2], t[3], t[5])
         except Exception as e:
             print("Bug with " + str(t[0]) + ": " + str(e))
             live_data[t[0]] = ('0','0','0','0')
@@ -143,7 +200,7 @@ if __name__ == '__main__':
     # Need to write this to JSON and we'll be golden
     drawdown = (live_data['S&P500'][-1] - live_data['S&P500'][0]) / live_data['S&P500'][0] * 100
     print("Drawdown:  {}".format(round(drawdown, 3)))
-    with open('/home/ec2-user/environment/market_dashboard/src/live_data.json', 'w') as fp:
+    with open('C:\\Users\\19739\\Python\\market_dashboard\\src\\live_data.json', 'w') as fp:
         json.dump(live_data, fp)
     
     print(datetime.datetime.now())
